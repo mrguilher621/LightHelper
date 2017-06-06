2738407fed9975bfc1cba926808fdea235377991
mrguilher621 <mrguilher621@gmail.com>
2017-06-06 18:24:43 -0300

    primeiro commit

---
+++ LightHelper/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using LightHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LightHelper.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private static IList<Cliente> clientes = new List<Cliente>()
+        {
+            new Cliente(){
+                ClienteID=1,
+                Nome = "Guilherme",
+                CPF = 123456789,
+                Telefone = 99914545,
+                Email = "mrguilher621@gmail.com"
+            },
+
+              new Cliente(){
+                ClienteID = 2,
+                Nome = "Lucas",
+                CPF = 987456123,
+                Telefone = 99914669,
+                Email = "lucas@gmail.com"
+            }
+
+        };
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            return View(clientes);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Cliente cliente)
+        {
+            clientes.Add(cliente);
+            cliente.ClienteID = clientes.Select(c => c.ClienteID).Max() + 1;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LightHelper/Models/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LightHelper.Models
+{
+    public class Cliente
+    {
+        public long? ClienteID { get; set; }
+        public string Nome { get; set; }
+        public int CPF { get; set; }
+        public int Telefone { get; set; }
+        public string Email { get; set; }
+    }
+}
